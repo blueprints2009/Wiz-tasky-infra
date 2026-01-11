2383f6600769d2c77b91ca774e08c0a66b085b05
fatimatulmuhsineen <fatimatul.m@pacewisdom.com>
2026-01-11 21:10:42 +0530

    add s3 statefile

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "s3" {
+    bucket         = "demo-terraform-state-bucket-1"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
